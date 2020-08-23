07c56789ce24793c64390c6c31fe0ec77d21f0b1
Jekplex <41015593+Jekplex@users.noreply.github.com>
2020-08-23 12:06:22 +0100

    1.0.3 Update - Customisation
    Customisation is complete. The player can now customise their player colour and the background image from a wide selection.

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BackgroundManager : MonoBehaviour
@@ -23,11 +24,77 @@ public class BackgroundManager : MonoBehaviour
 
     public GameObject[] Wallpapers;
 
+    public TMP_Dropdown WallpaperDropdown;
+
     // Methods for handling change of backgrounds.
     // These methods need to store/save preferences to PlayerPrefs 
     // so that on launch changes are kept to player's liking.
 
+    private void Start()
+    {
+        int tempVal = PlayerPrefs.GetInt("Wallpaper", 0);
+        //Debug.Log(PlayerPrefs.GetInt("Wallpaper", 0));
+        //Debug.Log(tempVal);
 
+        if (tempVal == 0) // Deep Space
+        {
+            currentWallpaper = Wallpaper.DeepSpace;
+        }
+        if (tempVal == 1) // OSU Pro
+        {
+            currentWallpaper = Wallpaper.OSUPro;
+        }
+        if (tempVal == 2) // Sombrero
+        {
+            currentWallpaper = Wallpaper.Sombrero;
+        }
+        if (tempVal == 3) // Purple
+        {
+            currentWallpaper = Wallpaper.Purple;
+        }
+        if (tempVal == 4) // Galaxy Brain
+        {
+            currentWallpaper = Wallpaper.GalaxyBrain;
+        }
+        if (tempVal == 5) // Mountains
+        {
+            currentWallpaper = Wallpaper.Mountains;
+        }
+        if (tempVal == 6) // Views
+        {
+            currentWallpaper = Wallpaper.Views;
+        }
+        if (tempVal == 7) // Jelly
+        {
+            currentWallpaper = Wallpaper.Jelly;
+        }
+        if (tempVal == 8) // Tiles
+        {
+            currentWallpaper = Wallpaper.Tiles;
+        }
+        if (tempVal == 9) // Clouds
+        {
+            currentWallpaper = Wallpaper.Clouds;
+        }
+
+        // Actual change happens here.
+        for (int i = 0; i < Wallpapers.Length; i++) // 10 is the number of actual wallpapers.
+        {
+            if (i == tempVal)
+            {
+                Wallpapers[i].SetActive(true);
+                PlayerPrefs.SetInt("Wallpaper", i);
+            }
+            else
+            {
+                Wallpapers[i].SetActive(false);
+            }
+        }
+
+        // We need this to effect the dropdown.
+        WallpaperDropdown.value = tempVal;
+
+    }
 
     public void SetBackground(int val)
     {
@@ -81,11 +148,12 @@ public class BackgroundManager : MonoBehaviour
         if (beforeWallpaper != currentWallpaper)
         {
             // Update Actual Wallpaper.
-            for (int i = 0; i < Wallpapers.Length; i++) // 9 is the number of actual wallpapers.
+            for (int i = 0; i < Wallpapers.Length; i++) // 10 is the number of actual wallpapers.
             {
                 if (i == val)
                 {
                     Wallpapers[i].SetActive(true);
+                    PlayerPrefs.SetInt("Wallpaper", i);
                 }
                 else
                 {
--- Assets/Scripts/GameCustomisation.cs
+++ Assets/Scripts/GameCustomisation.cs
@@ -2,52 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Drawing;
+using Color = UnityEngine.Color;
 
 public class GameCustomisation : MonoBehaviour
 {
+    // Regardless of script name...
+    // This script is focused on changing the player's colour.
 
-    // Background Image
-    
-    // Player Colour
+    // Background Manager is responsible for changing the wallpaper.
 
-    //public void HandleCustomisation(int val)
-    //{
-    //    switch (val)
-    //    {
-    //        case 0: // Deep Space
-    //            break;
-    //
-    //        case 1: // OSU Pro
-    //            break;
-    //
-    //        case 2: // Sombrero
-    //            break;
-    //
-    //        case 3: // Purple
-    //            break;
-    //
-    //        case 4: // Galaxy Brain
-    //            break;
-    //
-    //        case 5: // Mountains
-    //            break;
-    //
-    //        case 6: // Views
-    //            break;
-    //
-    //        case 7: // Jelly 
-    //            break;
-    //
-    //        case 8: // Tiles
-    //            break;
-    //
-    //        case 10: // Clouds
-    //            break;
-    //
-    //        default:
-    //            Debug.Log("Error");
-    //            break;
-    //    }
-    //}
+
+    private GameObject player;
+    private SpriteRenderer playerSR;
+
+    public TMP_Dropdown playerColourDropdown;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        playerSR = player.GetComponent<SpriteRenderer>();
+
+        int temp = PlayerPrefs.GetInt("PlayerColour", 0);
+        SetPlayerColour(temp);
+
+        // DropDown
+        playerColourDropdown.value = temp;
+    }
+
+    public void SetPlayerColour(int val)
+    {
+        // White + ROYGBIV
+
+        if (val == 0)
+        {
+            playerSR.color = Color.white;
+        }
+        if (val == 1)
+        {
+            playerSR.color = Color.red;
+        }
+        if (val == 2)
+        {
+            playerSR.color = new Color(1.0f, 0.64f, 0.0f); // Orange
+        }
+        if (val == 3)
+        {
+            playerSR.color = Color.yellow;
+        }
+        if (val == 4)
+        {
+            playerSR.color = Color.green;
+        }
+        if (val == 5)
+        {
+            playerSR.color = Color.blue;
+        }
+        if (val == 6)
+        {
+            playerSR.color = new Color(0.25f, 0.0f, 1.0f); // Indigo
+        }
+        if (val == 7)
+        {
+            playerSR.color = new Color(0.50f, 0.0f, 1.0f); // Violet
+        }
+        else
+        {
+            return;
+        }
+
+        PlayerPrefs.SetInt("PlayerColour", val);
+    }
+
 
 }
