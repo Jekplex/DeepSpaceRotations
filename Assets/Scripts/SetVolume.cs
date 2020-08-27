621f1e5db61402d10a08403205085495027e3390
Jekplex <41015593+Jekplex@users.noreply.github.com>
2020-08-27 16:33:13 +0100

    VolumeSlider Integration
    Add a volume slider for those who play with their windows volume maxed out >.>

---
+++ Assets/Scripts/SetVolume.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+// Reference
+// https://www.youtube.com/watch?v=xNHSGMKtlv4
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat("Volume", Mathf.Log10(sliderValue)*20); 
+    }
+}
