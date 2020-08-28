8496dbd3ddabd9f51dc9c94112604ee8a8f8bec5
Jekplex <41015593+Jekplex@users.noreply.github.com>
2020-08-28 17:46:43 +0100

    Update VolumeSlider
    VolumeSlider now stores/displays correct value to the player at all times.

--- Assets/Scripts/SetVolume.cs
+++ Assets/Scripts/SetVolume.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 // Reference
 // https://www.youtube.com/watch?v=xNHSGMKtlv4
@@ -10,8 +11,18 @@ public class SetVolume : MonoBehaviour
 {
     public AudioMixer mixer;
 
+    private Slider thisSlider;
+
+    private void Start()
+    {
+        thisSlider = GetComponent<Slider>();
+        thisSlider.value = PlayerPrefs.GetFloat("Volume", 1);
+    }
+
     public void SetLevel(float sliderValue)
     {
-        mixer.SetFloat("Volume", Mathf.Log10(sliderValue)*20); 
+        mixer.SetFloat("Volume", Mathf.Log10(sliderValue) * 20);
+
+        PlayerPrefs.SetFloat("Volume", sliderValue);
     }
 }
